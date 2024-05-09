8e8eae1516b08bad0d9f609f9698dcf945acdf58
TimKun55 <160082381+TimKun55@users.noreply.github.com>
2024-05-09 11:25:16 +0930

    Icon Updates and LoadMetatiles Fix
    Updated Icons for replaced Pokemon and current, applied LoadMetatiles wraps around past 128 blocks Fix

--- home/map.asm
+++ home/map.asm
@@ -163,10 +163,9 @@ _LoadMetatilesOrAttributes:
 	ld e, l
 	ld d, h
 	; Set hl to the address of the current metatile data ([wTilesetBlocksAddress] + (a) tiles).
-	add a
 	ld l, a
 	ld h, 0
-;	add hl, hl
+	add hl, hl
 	add hl, hl
 	add hl, hl
 	add hl, hl
