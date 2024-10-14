8943cffc1f0a5b37e2688020c5d505bec88a2097
TimKun55 <160082381+TimKun55@users.noreply.github.com>
2024-10-14 10:50:45 +1030

    Adjusted Cherrygrove City

--- constants/map_constants.asm
+++ constants/map_constants.asm
@@ -535,7 +535,7 @@ ENDM
 	newgroup CHERRYGROVE                                          ; 26
 	map_const ROUTE_30,                                    11, 27 ;  1
 	map_const ROUTE_31,                                    20,  9 ;  2
-	map_const CHERRYGROVE_CITY,                            20,  9 ;  3
+	map_const CHERRYGROVE_CITY,                            20, 10 ;  3
 	map_const CHERRYGROVE_MART,                             6,  4 ;  4
 	map_const CHERRYGROVE_POKECENTER_1F,                    6,  4 ;  5
 	map_const CHERRYGROVE_GYM_SPEECH_HOUSE,                 4,  4 ;  6
