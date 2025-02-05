97194d20e485027f1f975f9b8f177855ccfcefe5
TimKun55 <160082381+TimKun55@users.noreply.github.com>
2025-02-06 08:53:11 +1030

    Changed NUM_ROAMMONS to EQU 3
    For the possibility of having all 3 Beasts respawn after E4

--- constants/pokemon_data_constants.asm
+++ constants/pokemon_data_constants.asm
@@ -215,7 +215,7 @@ DEF WATER_WILDDATA_LENGTH EQU 2 + 1 + NUM_WATERMON * 4
 DEF FISHGROUP_DATA_LENGTH EQU 1 + 2 * 3
 
 DEF NUM_ROAMMON_MAPS EQU 16 ; RoamMaps table size (see data/wild/roammon_maps.asm)
-DEF NUM_ROAMMONS EQU 2 ; G/S is three, and wram still has 3 roamers
+DEF NUM_ROAMMONS EQU 3 ; G/S is three, and wram still has 3 roamers
 ; but  Crystal only has 2. please change this if you want to use the 3rd
 
 ; treemon sets
