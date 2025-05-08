0daa42cafac9fcbd154d479e7f71be27587ae186
TimKun55 <160082381+TimKun55@users.noreply.github.com>
2025-05-09 08:01:00 +0930

    Show item quantity already in Pack at Marts
    (TMs don't show quantity - already owned text)
    Crystal-Trove (c42aec5)

--- engine/items/mart.asm
+++ engine/items/mart.asm
@@ -729,7 +729,7 @@ MenuHeader_Buy:
 	dbw 0, wCurMartCount
 	dba PlaceMartMenuItemName
 	dba .PrintBCDPrices
-	dba UpdateItemDescription
+	dba MartPlaceInBagQuantity
 
 .PrintBCDPrices:
 	ld a, [wScrollingMenuCursorPosition]
@@ -747,6 +747,66 @@ MenuHeader_Buy:
 	add hl, bc
 	ld c, PRINTNUM_LEADINGZEROS | PRINTNUM_MONEY | 3
 	call PrintBCDNumber
+	ret
+
+MartPlaceInBagQuantity:
+	farcall UpdateItemDescription
+	farcall CheckItemPocket
+	ld a, [wItemAttributeValue]
+	cp ITEM
+	jr z, .get_item_pocket
+	cp BALL
+	jr z, .get_ball_pocket
+	cp MEDICINE
+	jr z, .get_medicine_pocket
+	cp BERRIES
+	jr z, .get_berry_pocket
+;	cp TM01
+	ret
+
+.get_item_pocket
+	ld hl, wNumItems
+	jr .check_bag
+
+.get_ball_pocket
+	ld hl, wNumBalls
+	jr .check_bag
+
+.get_medicine_pocket
+	ld hl, wNumMedicines
+	jr .check_bag
+
+.get_berry_pocket
+	ld hl, wNumBerries
+.check_bag
+	ld a, [wCurItem]
+	ld c, a
+	ld b, $0
+.loop
+	inc hl
+	ld a, [hli]
+	cp -1
+	jr z, .done
+	cp c
+	jr nz, .loop
+	ld a, [hl]
+	add b
+	ld b, a
+	jr nc, .loop
+	ld b, -1
+
+.done
+	ld a, b
+	sub 99
+	jr c, .done2
+	ld b, 99
+
+.done2
+	ld a, b
+	ld [wMenuSelectionQuantity], a
+	and a
+
+	farcall PlaceItemInBagQuantity
 	ret
 
 HerbShopLadyIntroText:
