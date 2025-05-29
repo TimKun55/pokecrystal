acdec91e0b16e748ade0c37780f07da75ad16ba1
TimKun55 <160082381+TimKun55@users.noreply.github.com>
2025-05-30 09:29:19 +0930

    Revert "Bugfix: Battle Tower attendant's overworld palette"
    This reverts commit b1a516f88872fd691b96541e40fc4091c3670b7e.

--- engine/events/battle_tower/battle_tower.asm
+++ engine/events/battle_tower/battle_tower.asm
@@ -1563,7 +1563,7 @@ LoadOpponentTrainerAndPokemonWithOTSprite:
 	ld a, [wBTTempOTSprite]
 	call GetSpritePalette
 	ld [wNeededPalIndex], a
-	ld de, wOBPals1 palette 2
+	ld de, wOBPals1 palette 1
 	farcall CopySpritePal
 	ret
 
