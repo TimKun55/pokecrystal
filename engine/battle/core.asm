7d6421f70a33bd6c4fa9aa2b19fb637cb581f554
TimKun55 <160082381+TimKun55@users.noreply.github.com>
2026-01-10 10:51:07 +1030

    Moved functions out of `core.asm` to `core2.asm`
    Based off of Johto-Frontier (4f3c416), (3debf1e), (ad18673), (dd58488), (cd62717), (b281f81), (b14f61f), (e62ac74), (bbc6416),

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -53,10 +53,8 @@ DoBattle:
 	jp z, LostBattle
 	call SafeLoadTempTilemapToTilemap
 	ld a, [wBattleType]
-	cp BATTLETYPE_DEBUG
-	jp z, .tutorial_debug
 	cp BATTLETYPE_TUTORIAL
-	jp z, .tutorial_debug
+	jp z, .tutorial
 	xor a
 	ld [wCurPartyMon], a
 .loop2
@@ -113,7 +111,7 @@ DoBattle:
 	call StartAutomaticBattleWeather
 	jp BattleTurn
 
-.tutorial_debug
+.tutorial
 	jp BattleMenu
 	
 StartAutomaticBattleWeather:
@@ -215,7 +213,6 @@ WildFled_EnemyFled_LinkBattleCanceled:
 
 BattleTurn:
 .loop
-	call Stubbed_Increments5_a89a
 	call CheckContestBattleOver
 	jp c, .quit
 
@@ -286,31 +283,13 @@ BattleTurn:
 .quit
 	ret
 
-Stubbed_Increments5_a89a:
-	ret
-	ld a, BANK(s5_a89a) ; MBC30 bank used by JP Crystal; inaccessible by MBC3
-	call OpenSRAM
-	ld hl, s5_a89a + 1 ; address of MBC30 bank
-	inc [hl]
-	jr nz, .finish
-	dec hl
-	inc [hl]
-	jr nz, .finish
-	dec [hl]
-	inc hl
-	dec [hl]
-
-.finish
-	call CloseSRAM
-	ret
-
 HandleBetweenTurnEffects:
 	ldh a, [hSerialConnectionStatus]
 	cp USING_EXTERNAL_CLOCK
 	jr z, .CheckEnemyFirst
 	call CheckFaint_PlayerThenEnemy
 	ret c
-	call HandleFutureSight
+	farcall HandleFutureSight
 	call CheckFaint_PlayerThenEnemy
 	ret c
 	call HandleWeather
@@ -324,7 +303,7 @@ HandleBetweenTurnEffects:
 .CheckEnemyFirst:
 	call CheckFaint_EnemyThenPlayer
 	ret c
-	call HandleFutureSight
+	farcall HandleFutureSight
 	call CheckFaint_EnemyThenPlayer
 	ret c
 	call HandleWeather
@@ -335,15 +314,12 @@ HandleBetweenTurnEffects:
 	ret c
 
 .NoMoreFaintingConditions:
-	call HandleLeftovers
-	call HandleLeppaberry
-	call HandleSafeguard
-	call HandleScreens
+	farcall Core2_NewTurnEndEffects
 	call HandleStatBoostingHeldItems
 	call HandleHealingItems
 	call UpdateBattleMonInParty
 	call LoadTilemapToTempTilemap
-	jp HandleEncore
+	newfarjp HandleEncore
 	
 HasAnyoneFainted:
 	call HasPlayerFainted
@@ -796,63 +772,6 @@ ParsePlayerAction:
 	res SUBSTATUS_RAGE, [hl]
 	xor a
 	ret
-
-HandleEncore:
-	ldh a, [hSerialConnectionStatus]
-	cp USING_EXTERNAL_CLOCK
-	jr z, .player_1
-	call .do_player
-	jr .do_enemy
-
-.player_1
-	call .do_enemy
-.do_player
-	ld hl, wPlayerSubStatus5
-	bit SUBSTATUS_ENCORED, [hl]
-	ret z
-	ld a, [wPlayerEncoreCount]
-	dec a
-	ld [wPlayerEncoreCount], a
-	jr z, .end_player_encore
-	ld hl, wBattleMonPP
-	ld a, [wCurMoveNum]
-	ld c, a
-	ld b, 0
-	add hl, bc
-	ld a, [hl]
-	and PP_MASK
-	ret nz
-
-.end_player_encore
-	ld hl, wPlayerSubStatus5
-	res SUBSTATUS_ENCORED, [hl]
-	call SetEnemyTurn
-	ld hl, BattleText_TargetsEncoreEnded
-	jp StdBattleTextbox
-
-.do_enemy
-	ld hl, wEnemySubStatus5
-	bit SUBSTATUS_ENCORED, [hl]
-	ret z
-	ld a, [wEnemyEncoreCount]
-	dec a
-	ld [wEnemyEncoreCount], a
-	jr z, .end_enemy_encore
-	ld hl, wEnemyMonPP
-	ld a, [wCurEnemyMoveNum]
-	ld c, a
-	ld b, 0
-	add hl, bc
-	ld a, [hl]
-	and PP_MASK
-	ret nz
-
-.end_enemy_encore
-	ld hl, wEnemySubStatus5
-	res SUBSTATUS_ENCORED, [hl]
-	call SetPlayerTurn
-	ld hl, BattleText_TargetsEncoreEnded
-	jp StdBattleTextbox
 
 TryEnemyFlee:
 	ld a, [wBattleMode]
@@ -1311,353 +1230,6 @@ SwitchTurnCore:
 	xor 1
 	ldh [hBattleTurn], a
 	ret
-
-HandleLeftovers:
-	ldh a, [hSerialConnectionStatus]
-	cp USING_EXTERNAL_CLOCK
-	jr z, .DoEnemyFirst
-	call SetPlayerTurn
-	call .do_it
-	call SetEnemyTurn
-	jp .do_it
-
-.DoEnemyFirst:
-	call SetEnemyTurn
-	call .do_it
-	call SetPlayerTurn
-.do_it
-
-	callfar GetUserItem
-	ld a, [hl]
-	ld [wNamedObjectIndex], a
-	call GetItemName
-	ld a, b
-	cp HELD_LEFTOVERS
-	ret nz
-
-	ld hl, wBattleMonHP
-	ldh a, [hBattleTurn]
-	and a
-	jr z, .got_hp
-	ld hl, wEnemyMonHP
-
-.got_hp
-; Don't restore if we're already at max HP
-	ld a, [hli]
-	ld b, a
-	ld a, [hli]
-	ld c, a
-	ld a, [hli]
-	cp b
-	jr nz, .restore
-	ld a, [hl]
-	cp c
-	ret z
-
-.restore
-	call GetSixteenthMaxHP
-	call SwitchTurnCore
-	call RestoreHP
-	ld hl, BattleText_TargetRecoveredWithItem
-	jp StdBattleTextbox
-
-HandleLeppaberry:
-	ldh a, [hSerialConnectionStatus]
-	cp USING_EXTERNAL_CLOCK
-	jr z, .DoEnemyFirst
-	call SetPlayerTurn
-	call .do_it
-	call SetEnemyTurn
-	jp .do_it
-
-.DoEnemyFirst:
-	call SetEnemyTurn
-	call .do_it
-	call SetPlayerTurn
-
-.do_it
-	callfar GetUserItem
-	ld a, b
-	cp HELD_RESTORE_PP
-	jr nz, .quit
-	ld hl, wPartyMon1PP
-	ld a, [wCurBattleMon]
-	call GetPartyLocation
-	ld d, h
-	ld e, l
-	ld hl, wPartyMon1Moves
-	ld a, [wCurBattleMon]
-	call GetPartyLocation
-	ldh a, [hBattleTurn]
-	and a
-	jr z, .wild
-	ld de, wWildMonPP
-	ld hl, wWildMonMoves
-	ld a, [wBattleMode]
-	dec a
-	jr z, .wild
-	ld hl, wOTPartyMon1PP
-	ld a, [wCurOTMon]
-	call GetPartyLocation
-	ld d, h
-	ld e, l
-	ld hl, wOTPartyMon1Moves
-	ld a, [wCurOTMon]
-	call GetPartyLocation
-
-.wild
-	ld c, $0
-.loop
-	ld a, [hl]
-	and a
-	jr z, .quit
-	ld a, [de]
-	and PP_MASK
-	jr z, .restore
-	inc hl
-	inc de
-	inc c
-	ld a, c
-	cp NUM_MOVES
-	jr nz, .loop
-
-.quit
-	ret
-
-.restore
-	; lousy hack
-	ld a, [hl]
-	cp SKETCH
-	ld b, 1
-	jr z, .sketch
-	ld b, 5
-.sketch
-	ld a, [de]
-	add b
-	ld [de], a
-	push bc
-	push bc
-	ld a, [hl]
-	ld [wTempByteValue], a
-	ld de, wBattleMonMoves - 1
-	ld hl, wBattleMonPP
-	ldh a, [hBattleTurn]
-	and a
-	jr z, .player_pp
-	ld de, wEnemyMonMoves - 1
-	ld hl, wEnemyMonPP
-.player_pp
-	inc de
-	pop bc
-	ld b, 0
-	add hl, bc
-	push hl
-	ld h, d
-	ld l, e
-	add hl, bc
-	pop de
-	pop bc
-
-	ld a, [wTempByteValue]
-	cp [hl]
-	jr nz, .skip_checks
-	ldh a, [hBattleTurn]
-	and a
-	ld a, [wPlayerSubStatus5]
-	jr z, .check_transform
-	ld a, [wEnemySubStatus5]
-.check_transform
-	bit SUBSTATUS_TRANSFORMED, a
-	jr nz, .skip_checks
-	ld a, [de]
-	add b
-	ld [de], a
-.skip_checks
-	callfar GetUserItem
-	ld a, [hl]
-	ld [wNamedObjectIndex], a
-	xor a
-	ld [hl], a
-	call GetPartymonItem
-	ldh a, [hBattleTurn]
-	and a
-	jr z, .consume_item
-	ld a, [wBattleMode]
-	dec a
-	jr z, .skip_consumption
-	call GetOTPartymonItem
-
-.consume_item
-	xor a
-	ld [hl], a
-
-.skip_consumption
-	call GetItemName
-	call SwitchTurnCore
-	call ItemRecoveryAnim
-	call SwitchTurnCore
-	ld hl, BattleText_UserRecoveredPPUsing
-	jp StdBattleTextbox
-
-HandleFutureSight:
-	ldh a, [hSerialConnectionStatus]
-	cp USING_EXTERNAL_CLOCK
-	jr z, .enemy_first
-	call SetPlayerTurn
-	call .do_it
-	call SetEnemyTurn
-	jp .do_it
-
-.enemy_first
-	call SetEnemyTurn
-	call .do_it
-	call SetPlayerTurn
-
-.do_it
-	ld hl, wPlayerFutureSightCount
-	ldh a, [hBattleTurn]
-	and a
-	jr z, .okay
-	ld hl, wEnemyFutureSightCount
-
-.okay
-	ld a, [hl]
-	and a
-	ret z
-	dec a
-	ld [hl], a
-	cp $1
-	ret nz
-
-	ld hl, BattleText_TargetWasHitByFutureSight
-	call StdBattleTextbox
-
-	ld a, BATTLE_VARS_MOVE
-	call GetBattleVarAddr
-	push af
-	ld a, FUTURE_SIGHT
-	ld [hl], a
-
-	callfar UpdateMoveData
-	xor a
-	ld [wAttackMissed], a
-	ld [wAlreadyDisobeyed], a
-	ld a, EFFECTIVE
-	ld [wTypeModifier], a
-	callfar DoMove
-	xor a
-	ld [wCurDamage], a
-	ld [wCurDamage + 1], a
-
-	ld a, BATTLE_VARS_MOVE
-	call GetBattleVarAddr
-	pop af
-	ld [hl], a
-
-	call UpdateBattleMonInParty
-	jp UpdateEnemyMonInParty
-
-HandleSafeguard:
-	ldh a, [hSerialConnectionStatus]
-	cp USING_EXTERNAL_CLOCK
-	jr z, .player1
-	call .CheckPlayer
-	jr .CheckEnemy
-
-.player1
-	call .CheckEnemy
-.CheckPlayer:
-	ld a, [wPlayerScreens]
-	bit SCREENS_SAFEGUARD, a
-	ret z
-	ld hl, wPlayerSafeguardCount
-	dec [hl]
-	ret nz
-	res SCREENS_SAFEGUARD, a
-	ld [wPlayerScreens], a
-	xor a
-	jr .print
-
-.CheckEnemy:
-	ld a, [wEnemyScreens]
-	bit SCREENS_SAFEGUARD, a
-	ret z
-	ld hl, wEnemySafeguardCount
-	dec [hl]
-	ret nz
-	res SCREENS_SAFEGUARD, a
-	ld [wEnemyScreens], a
-	ld a, $1
-
-.print
-	ldh [hBattleTurn], a
-	ld hl, BattleText_SafeguardFaded
-	jp StdBattleTextbox
-
-HandleScreens:
-	ldh a, [hSerialConnectionStatus]
-	cp USING_EXTERNAL_CLOCK
-	jr z, .Both
-	call .CheckPlayer
-	jr .CheckEnemy
-
-.Both:
-	call .CheckEnemy
-
-.CheckPlayer:
-	call SetPlayerTurn
-	ld de, .Your
-	call .Copy
-	ld hl, wPlayerScreens
-	ld de, wPlayerLightScreenCount
-	jr .TickScreens
-
-.CheckEnemy:
-	call SetEnemyTurn
-	ld de, .Enemy
-	call .Copy
-	ld hl, wEnemyScreens
-	ld de, wEnemyLightScreenCount
-
-.TickScreens:
-	bit SCREENS_LIGHT_SCREEN, [hl]
-	call nz, .LightScreenTick
-	bit SCREENS_REFLECT, [hl]
-	call nz, .ReflectTick
-	ret
-
-.Copy:
-	ld hl, wStringBuffer1
-	jp CopyName2
-
-.Your:
-	db "Your@"
-.Enemy:
-	db "Enemy@"
-
-.LightScreenTick:
-	ld a, [de]
-	dec a
-	ld [de], a
-	ret nz
-	res SCREENS_LIGHT_SCREEN, [hl]
-	push hl
-	push de
-	ld hl, BattleText_MonsLightScreenFell
-	call StdBattleTextbox
-	pop de
-	pop hl
-	ret
-
-.ReflectTick:
-	inc de
-	ld a, [de]
-	dec a
-	ld [de], a
-	ret nz
-	res SCREENS_REFLECT, [hl]
-	ld hl, BattleText_MonsReflectFaded
-	jp StdBattleTextbox
 
 HandleWeather:
 	ld a, [wBattleWeather]
@@ -3768,8 +3340,6 @@ CheckIfCurPartyMonIsFitToFight:
 TryToRunAwayFromBattle:
 ; Run away from battle, with or without item
 	ld a, [wBattleType]
-	cp BATTLETYPE_DEBUG
-	jp z, .can_escape
 	cp BATTLETYPE_CONTEST
 	jp z, .can_escape
 	cp BATTLETYPE_TRAP
@@ -4136,7 +3706,7 @@ SendOutPlayerMon:
 	ld [wLastPlayerCounterMove], a
 	ld [wLastEnemyCounterMove], a
 	ld [wLastPlayerMove], a
-	call CheckAmuletCoin
+	farcall CheckAmuletCoin
 	call FinishBattleAnim
 	xor a
 	ld [wEnemyWrapCount], a
@@ -4756,7 +4326,6 @@ CheckDanger:
 PrintPlayerHUD:
 	ld de, wBattleMonNickname
 	hlcoord 10, 7
-	call Battle_DummyFunction
 	call PlaceString
 
 	push bc
@@ -4840,7 +4409,6 @@ DrawEnemyHUD:
 	call GetBaseData
 	ld de, wEnemyMonNickname
 	hlcoord 1, 0
-	call Battle_DummyFunction
 	call PlaceString
 	ld h, b
 	ld l, c
@@ -4966,18 +4534,12 @@ UpdateHPPal:
 	ret z
 	jp FinishBattleAnim
 
-Battle_DummyFunction:
-; called before placing either battler's nickname in the HUD
-	ret
-
 BattleMenu:
 	xor a
 	ldh [hBGMapMode], a
 	call LoadTempTilemapToTilemap
 
 	ld a, [wBattleType]
-	cp BATTLETYPE_DEBUG
-	jr z, .ok
 	cp BATTLETYPE_TUTORIAL
 	jr z, .ok
 	call EmptyBattleTextbox
@@ -4986,7 +4548,7 @@ BattleMenu:
 	call LoadTilemapToTempTilemap
 .ok
 
-	call GetWeatherImage
+	farcall GetWeatherImage
 
 .loop
 	ld a, [wBattleType]
@@ -5427,17 +4989,6 @@ BattleMenu_Run:
 	and a ; BATTLEPLAYERACTION_USEMOVE?
 	ret nz
 	jp BattleMenu
-
-CheckAmuletCoin:
-	ld a, [wBattleMonItem]
-	ld b, a
-	callfar GetItemHeldEffect
-	ld a, b
-	cp HELD_AMULET_COIN
-	ret nz
-	ld a, 1
-	ld [wAmuletCoin], a
-	ret
 
 MoveSelectionScreen:
 	call IsMobileBattle
@@ -8333,7 +7884,7 @@ StartBattle:
 	push af
 	call BattleIntro
 	call DoBattle
-	call ExitBattle
+	farcall ExitBattle
 	pop af
 	ld [wTimeOfDayPal], a
 	scf
@@ -8553,33 +8104,6 @@ FillEnemyMovesFromMoveIndicesBuffer: ; unreferenced
 	pop bc
 	dec b
 	jr nz, .clear
-	ret
-
-ExitBattle:
-	farcall UpdatePartyStats
-	call .HandleEndOfBattle
-	call CleanUpBattleRAM
-	ret
-
-.HandleEndOfBattle:
-	ld a, [wLinkMode]
-	and a
-	jr z, .not_linked
-	call ShowLinkBattleParticipantsAfterEnd
-	ld c, 150
-	call DelayFrames
-	call DisplayLinkBattleResult
-	ret
-
-.not_linked
-	ld a, [wBattleResult]
-	and $f
-	ret nz
-	call CheckPayDay
-	xor a
-	ld [wForceEvolution], a
-	predef EvolveAfterBattle
-	farcall GivePokerusAndConvertBerries
 	ret
 
 CleanUpBattleRAM:
@@ -9268,41 +8792,8 @@ InitBattleDisplay:
 	ret
 
 .InitBackPic:
-	call GetTrainerBackpic
+	farcall GetTrainerBackpic
 	call CopyBackpic
-	ret
-
-GetTrainerBackpic:
-; Load the player character's backpic (6x6) into VRAM starting from vTiles2 tile $31.
-
-; Special exception for Lyra.
-	ld b, BANK(LyraBackpic)
-	ld hl, LyraBackpic
-	ld a, [wBattleType]
-	cp BATTLETYPE_TUTORIAL
-	jr z, .Decompress
-
-; What gender are we?
-	ld a, [wPlayerSpriteSetupFlags]
-	bit PLAYERSPRITESETUP_FEMALE_TO_MALE_F, a
-	jr nz, .Ethan
-	ld a, [wPlayerGender]
-	bit PLAYERGENDER_FEMALE_F, a
-	jr z, .Ethan
-
-; It's a girl.
-	farcall GetKrisBackpic
-	ret
-
-.Ethan:
-; It's a boy.
-	ld b, BANK(EthanBackpic)
-	ld hl, EthanBackpic
-
-.Decompress:
-	ld de, vTiles2 tile $31
-	ld c, 7 * 7
-	predef DecompressGet2bpp
 	ret
 
 CopyBackpic:
@@ -9445,57 +8936,3 @@ BattleStartMessage:
 	farcall Mobile_PrintOpponentBattleMessage
 
 	ret
-	
-GetWeatherImage:
-	ld a, [wBattleWeather]
-	ld de, ClearWeatherImage
-	lb bc, PAL_BATTLE_OB_BLUE, 4
-	cp WEATHER_NONE
-	jr z, .done
-	ld de, RainWeatherImage
-	lb bc, PAL_BATTLE_OB_BLUE, 4
-	cp WEATHER_RAIN
-	jr z, .done
-	ld de, HailWeatherImage
-	lb bc, PAL_BATTLE_OB_BLUE, 4
-	cp WEATHER_HAIL
-	jr z, .done
-	ld de, SunWeatherImage
-	ld b, PAL_BATTLE_OB_YELLOW
-	cp WEATHER_SUN
-	jr z, .done
-	ld de, SandstormWeatherImage
-	ld b, PAL_BATTLE_OB_BROWN
-	cp WEATHER_SANDSTORM
-	ret nz
-	
-.done
-	push bc
-	ld b, BANK(WeatherImages) ; c = 4
-	ld hl, vTiles0
-	call Request2bpp
-	pop bc
-	ld hl, wShadowOAMSprite00
-	ld de, .WeatherImageOAMData
-.loop
-	ld a, [de]
-	inc de
-	ld [hli], a
-	ld a, [de]
-	inc de
-	ld [hli], a
-	dec c
-	ld a, c
-	ld [hli], a
-	ld a, b
-	ld [hli], a
-	jr nz, .loop
-	ret
-
-.WeatherImageOAMData
-; positions are backwards since
-; we load them in reverse order
-	db $90, $1c ; y/x - bottom right
-	db $90, $14 ; y/x - bottom left
-	db $88, $1c ; y/x - top right
-	db $88, $14 ; y/x - top left
---
+++ engine/battle/core2.asm
@@ -0,0 +1,535 @@
+Core2_NewTurnEndEffects:
+	call HandleLeftovers
+	call HandleLeppaberry
+	call HandleSafeguard
+	call HandleScreens
+	ret
+
+HandleLeftovers:
+	ldh a, [hSerialConnectionStatus]
+	cp USING_EXTERNAL_CLOCK
+	jr z, .DoEnemyFirst
+	call SetPlayerTurn
+	call .do_it
+	call SetEnemyTurn
+	jp .do_it
+
+.DoEnemyFirst:
+	call SetEnemyTurn
+	call .do_it
+	call SetPlayerTurn
+.do_it
+
+	callfar GetUserItem
+	ld a, [hl]
+	ld [wNamedObjectIndex], a
+	call GetItemName
+	ld a, b
+	cp HELD_LEFTOVERS
+	ret nz
+
+	ld hl, wBattleMonHP
+	ldh a, [hBattleTurn]
+	and a
+	jr z, .got_hp
+	ld hl, wEnemyMonHP
+
+.got_hp
+; Don't restore if we're already at max HP
+	ld a, [hli]
+	ld b, a
+	ld a, [hli]
+	ld c, a
+	ld a, [hli]
+	cp b
+	jr nz, .restore
+	ld a, [hl]
+	cp c
+	ret z
+
+.restore
+	call GetSixteenthMaxHP
+	call SwitchTurnCore
+	call RestoreHP
+	ld hl, BattleText_TargetRecoveredWithItem
+	jp StdBattleTextbox
+
+HandleLeppaberry:
+	ldh a, [hSerialConnectionStatus]
+	cp USING_EXTERNAL_CLOCK
+	jr z, .DoEnemyFirst
+	call SetPlayerTurn
+	call .do_it
+	call SetEnemyTurn
+	jp .do_it
+
+.DoEnemyFirst:
+	call SetEnemyTurn
+	call .do_it
+	call SetPlayerTurn
+
+.do_it
+	callfar GetUserItem
+	ld a, b
+	cp HELD_RESTORE_PP
+	jr nz, .quit
+	ld hl, wPartyMon1PP
+	ld a, [wCurBattleMon]
+	call GetPartyLocation
+	ld d, h
+	ld e, l
+	ld hl, wPartyMon1Moves
+	ld a, [wCurBattleMon]
+	call GetPartyLocation
+	ldh a, [hBattleTurn]
+	and a
+	jr z, .wild
+	ld de, wWildMonPP
+	ld hl, wWildMonMoves
+	ld a, [wBattleMode]
+	dec a
+	jr z, .wild
+	ld hl, wOTPartyMon1PP
+	ld a, [wCurOTMon]
+	call GetPartyLocation
+	ld d, h
+	ld e, l
+	ld hl, wOTPartyMon1Moves
+	ld a, [wCurOTMon]
+	call GetPartyLocation
+
+.wild
+	ld c, $0
+.loop
+	ld a, [hl]
+	and a
+	jr z, .quit
+	ld a, [de]
+	and PP_MASK
+	jr z, .restore
+	inc hl
+	inc de
+	inc c
+	ld a, c
+	cp NUM_MOVES
+	jr nz, .loop
+
+.quit
+	ret
+
+.restore
+	; lousy hack
+	ld a, [hl]
+	cp SKETCH
+	ld b, 1
+	jr z, .sketch
+	ld b, 5
+.sketch
+	ld a, [de]
+	add b
+	ld [de], a
+	push bc
+	push bc
+	ld a, [hl]
+	ld [wTempByteValue], a
+	ld de, wBattleMonMoves - 1
+	ld hl, wBattleMonPP
+	ldh a, [hBattleTurn]
+	and a
+	jr z, .player_pp
+	ld de, wEnemyMonMoves - 1
+	ld hl, wEnemyMonPP
+.player_pp
+	inc de
+	pop bc
+	ld b, 0
+	add hl, bc
+	push hl
+	ld h, d
+	ld l, e
+	add hl, bc
+	pop de
+	pop bc
+
+	ld a, [wTempByteValue]
+	cp [hl]
+	jr nz, .skip_checks
+	ldh a, [hBattleTurn]
+	and a
+	ld a, [wPlayerSubStatus5]
+	jr z, .check_transform
+	ld a, [wEnemySubStatus5]
+.check_transform
+	bit SUBSTATUS_TRANSFORMED, a
+	jr nz, .skip_checks
+	ld a, [de]
+	add b
+	ld [de], a
+.skip_checks
+	callfar GetUserItem
+	ld a, [hl]
+	ld [wNamedObjectIndex], a
+	xor a
+	ld [hl], a
+	call GetPartymonItem
+	ldh a, [hBattleTurn]
+	and a
+	jr z, .consume_item
+	ld a, [wBattleMode]
+	dec a
+	jr z, .skip_consumption
+	call GetOTPartymonItem
+
+.consume_item
+	xor a
+	ld [hl], a
+
+.skip_consumption
+	call GetItemName
+	call SwitchTurnCore
+	call ItemRecoveryAnim
+	call SwitchTurnCore
+	ld hl, BattleText_UserRecoveredPPUsing
+	jp StdBattleTextbox
+
+HandleSafeguard:
+	ldh a, [hSerialConnectionStatus]
+	cp USING_EXTERNAL_CLOCK
+	jr z, .player1
+	call .CheckPlayer
+	jr .CheckEnemy
+
+.player1
+	call .CheckEnemy
+.CheckPlayer:
+	ld a, [wPlayerScreens]
+	bit SCREENS_SAFEGUARD, a
+	ret z
+	ld hl, wPlayerSafeguardCount
+	dec [hl]
+	ret nz
+	res SCREENS_SAFEGUARD, a
+	ld [wPlayerScreens], a
+	xor a
+	jr .print
+
+.CheckEnemy:
+	ld a, [wEnemyScreens]
+	bit SCREENS_SAFEGUARD, a
+	ret z
+	ld hl, wEnemySafeguardCount
+	dec [hl]
+	ret nz
+	res SCREENS_SAFEGUARD, a
+	ld [wEnemyScreens], a
+	ld a, $1
+
+.print
+	ldh [hBattleTurn], a
+	ld hl, BattleText_SafeguardFaded
+	jp StdBattleTextbox
+
+HandleScreens:
+	ldh a, [hSerialConnectionStatus]
+	cp USING_EXTERNAL_CLOCK
+	jr z, .Both
+	call .CheckPlayer
+	jr .CheckEnemy
+
+.Both:
+	call .CheckEnemy
+
+.CheckPlayer:
+	call SetPlayerTurn
+	ld de, .Your
+	call .Copy
+	ld hl, wPlayerScreens
+	ld de, wPlayerLightScreenCount
+	jr .TickScreens
+
+.CheckEnemy:
+	call SetEnemyTurn
+	ld de, .Enemy
+	call .Copy
+	ld hl, wEnemyScreens
+	ld de, wEnemyLightScreenCount
+
+.TickScreens:
+	bit SCREENS_LIGHT_SCREEN, [hl]
+	call nz, .LightScreenTick
+	bit SCREENS_REFLECT, [hl]
+	call nz, .ReflectTick
+	ret
+
+.Copy:
+	ld hl, wStringBuffer1
+	jp CopyName2
+
+.Your:
+	db "Your@"
+.Enemy:
+	db "Enemy@"
+
+.LightScreenTick:
+	ld a, [de]
+	dec a
+	ld [de], a
+	ret nz
+	res SCREENS_LIGHT_SCREEN, [hl]
+	push hl
+	push de
+	ld hl, BattleText_MonsLightScreenFell
+	call StdBattleTextbox
+	pop de
+	pop hl
+	ret
+
+.ReflectTick:
+	inc de
+	ld a, [de]
+	dec a
+	ld [de], a
+	ret nz
+	res SCREENS_REFLECT, [hl]
+	ld hl, BattleText_MonsReflectFaded
+	jp StdBattleTextbox
+
+ExitBattle:
+	farcall UpdatePartyStats
+	call .HandleEndOfBattle
+	call CleanUpBattleRAM
+	ret
+
+.HandleEndOfBattle:
+	ld a, [wLinkMode]
+	and a
+	jr z, .not_linked
+	call ShowLinkBattleParticipantsAfterEnd
+	ld c, 150
+	call DelayFrames
+	call DisplayLinkBattleResult
+	ret
+
+.not_linked
+	ld a, [wBattleResult]
+	and $f
+	ret nz
+	call CheckPayDay
+	xor a
+	ld [wForceEvolution], a
+	predef EvolveAfterBattle
+	farcall GivePokerusAndConvertBerries
+	ret
+
+GetTrainerBackpic:
+; Load the player character's backpic (6x6) into VRAM starting from vTiles2 tile $31.
+
+; Special exception for Lyra.
+	ld b, BANK(LyraBackpic)
+	ld hl, LyraBackpic
+	ld a, [wBattleType]
+	cp BATTLETYPE_TUTORIAL
+	jr z, .Decompress
+
+; What gender are we?
+	ld a, [wPlayerSpriteSetupFlags]
+	bit PLAYERSPRITESETUP_FEMALE_TO_MALE_F, a
+	jr nz, .Ethan
+	ld a, [wPlayerGender]
+	bit PLAYERGENDER_FEMALE_F, a
+	jr z, .Ethan
+
+; It's a girl.
+	farcall GetKrisBackpic
+	ret
+
+.Ethan:
+; It's a boy.
+	ld b, BANK(EthanBackpic)
+	ld hl, EthanBackpic
+
+.Decompress:
+	ld de, vTiles2 tile $31
+	ld c, 7 * 7
+	predef DecompressGet2bpp
+	ret
+
+HandleEncore:
+	ldh a, [hSerialConnectionStatus]
+	cp USING_EXTERNAL_CLOCK
+	jr z, .player_1
+	call .do_player
+	jr .do_enemy
+
+.player_1
+	call .do_enemy
+.do_player
+	ld hl, wPlayerSubStatus5
+	bit SUBSTATUS_ENCORED, [hl]
+	ret z
+	ld a, [wPlayerEncoreCount]
+	dec a
+	ld [wPlayerEncoreCount], a
+	jr z, .end_player_encore
+	ld hl, wBattleMonPP
+	ld a, [wCurMoveNum]
+	ld c, a
+	ld b, 0
+	add hl, bc
+	ld a, [hl]
+	and PP_MASK
+	ret nz
+
+.end_player_encore
+	ld hl, wPlayerSubStatus5
+	res SUBSTATUS_ENCORED, [hl]
+	call SetEnemyTurn
+	ld hl, BattleText_TargetsEncoreEnded
+	jp StdBattleTextbox
+
+.do_enemy
+	ld hl, wEnemySubStatus5
+	bit SUBSTATUS_ENCORED, [hl]
+	ret z
+	ld a, [wEnemyEncoreCount]
+	dec a
+	ld [wEnemyEncoreCount], a
+	jr z, .end_enemy_encore
+	ld hl, wEnemyMonPP
+	ld a, [wCurEnemyMoveNum]
+	ld c, a
+	ld b, 0
+	add hl, bc
+	ld a, [hl]
+	and PP_MASK
+	ret nz
+
+.end_enemy_encore
+	ld hl, wEnemySubStatus5
+	res SUBSTATUS_ENCORED, [hl]
+	call SetPlayerTurn
+	ld hl, BattleText_TargetsEncoreEnded
+	jp StdBattleTextbox
+
+CheckAmuletCoin:
+	ld a, [wBattleMonItem]
+	ld b, a
+	callfar GetItemHeldEffect
+	ld a, b
+	cp HELD_AMULET_COIN
+	ret nz
+	ld a, 1
+	ld [wAmuletCoin], a
+	ret
+
+HandleFutureSight:
+	ldh a, [hSerialConnectionStatus]
+	cp USING_EXTERNAL_CLOCK
+	jr z, .enemy_first
+	call SetPlayerTurn
+	call .do_it
+	call SetEnemyTurn
+	jp .do_it
+
+.enemy_first
+	call SetEnemyTurn
+	call .do_it
+	call SetPlayerTurn
+
+.do_it
+	ld hl, wPlayerFutureSightCount
+	ldh a, [hBattleTurn]
+	and a
+	jr z, .okay
+	ld hl, wEnemyFutureSightCount
+
+.okay
+	ld a, [hl]
+	and a
+	ret z
+	dec a
+	ld [hl], a
+	cp $1
+	ret nz
+
+	ld hl, BattleText_TargetWasHitByFutureSight
+	call StdBattleTextbox
+
+	ld a, BATTLE_VARS_MOVE
+	call GetBattleVarAddr
+	push af
+	ld a, FUTURE_SIGHT
+	ld [hl], a
+
+	callfar UpdateMoveData
+	xor a
+	ld [wAttackMissed], a
+	ld [wAlreadyDisobeyed], a
+	ld a, EFFECTIVE
+	ld [wTypeModifier], a
+	callfar DoMove
+	xor a
+	ld [wCurDamage], a
+	ld [wCurDamage + 1], a
+
+	ld a, BATTLE_VARS_MOVE
+	call GetBattleVarAddr
+	pop af
+	ld [hl], a
+
+	call UpdateBattleMonInParty
+	jp UpdateEnemyMonInParty
+
+GetWeatherImage:
+	ld a, [wBattleWeather]
+	ld de, ClearWeatherImage
+	lb bc, PAL_BATTLE_OB_BLUE, 4
+	cp WEATHER_NONE
+	jr z, .done
+	ld de, RainWeatherImage
+	lb bc, PAL_BATTLE_OB_BLUE, 4
+	cp WEATHER_RAIN
+	jr z, .done
+	ld de, HailWeatherImage
+	lb bc, PAL_BATTLE_OB_BLUE, 4
+	cp WEATHER_HAIL
+	jr z, .done
+	ld de, SunWeatherImage
+	ld b, PAL_BATTLE_OB_YELLOW
+	cp WEATHER_SUN
+	jr z, .done
+	ld de, SandstormWeatherImage
+	ld b, PAL_BATTLE_OB_BROWN
+	cp WEATHER_SANDSTORM
+	ret nz
+	
+.done
+	push bc
+	ld b, BANK(WeatherImages) ; c = 4
+	ld hl, vTiles0
+	call Request2bpp
+	pop bc
+	ld hl, wShadowOAMSprite00
+	ld de, .WeatherImageOAMData
+.loop
+	ld a, [de]
+	inc de
+	ld [hli], a
+	ld a, [de]
+	inc de
+	ld [hli], a
+	dec c
+	ld a, c
+	ld [hli], a
+	ld a, b
+	ld [hli], a
+	jr nz, .loop
+	ret
+
+.WeatherImageOAMData
+; positions are backwards since
+; we load them in reverse order
+	db $90, $1c ; y/x - bottom right
+	db $90, $14 ; y/x - bottom left
+	db $88, $1c ; y/x - top right
+	db $88, $14 ; y/x - top left
