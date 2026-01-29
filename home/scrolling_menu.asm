fc550242a7927aa88b43a1efb0d8cc86bdafd645
TimKun55 <160082381+TimKun55@users.noreply.github.com>
2026-01-30 08:45:58 +1030

    Revert "Use combined joypad masks"
    This reverts commit 85f7cc1e528b310db784ca4795b71f40e00bd7ca.

--- home/scrolling_menu.asm
+++ home/scrolling_menu.asm
@@ -52,10 +52,10 @@ JoyTextDelay_ForcehJoyDown::
 	ldh [hInMenu], a
 
 	ldh a, [hJoyLast]
-	and PAD_CTRL_PAD
+	and PAD_RIGHT + PAD_LEFT + PAD_UP + PAD_DOWN
 	ld c, a
 	ldh a, [hJoyPressed]
-	and PAD_BUTTONS
+	and PAD_A + PAD_B + PAD_SELECT + PAD_START
 	or c
 	ld c, a
 	ret
